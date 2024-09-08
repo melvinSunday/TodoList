394a79091d062b51ac651899e2619b788aebbbd5
Melvin Giovanni Domingo <domingotaiga@gmail.com>
2024-09-08 17:22:28 +0800

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Todo App',
+        short_name: 'TodoApp',
+        description: 'A simple todo app built with Vite and React.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/todo_icon_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/todo_icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
-      'antd': 'antd/es', 
+      'antd': 'antd/es',
     },
   },
 });
